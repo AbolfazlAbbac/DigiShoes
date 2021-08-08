a970d5d01cf3b9561e078087b757ae76cba21625
abolfazlabbasi185@gmail.com <abolfazlabbasi185@gmail.com>
2021-08-08 16:55:14 +0430

    Rv Since RecyclerView whitout Ui

--- app/src/main/java/com/example/digishoes/feature/main/MainFragment.kt
+++ app/src/main/java/com/example/digishoes/feature/main/MainFragment.kt
@@ -3,22 +3,32 @@ package com.example.digishoes.feature.main
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.viewpager2.widget.ViewPager2
 import com.example.digishoes.R
 import com.example.digishoes.common.NikeFragment
+import com.example.digishoes.databinding.ActivityMainBinding.inflate
+import com.example.digishoes.databinding.FragmentCartBinding
+import com.example.digishoes.databinding.FragmentMainBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
+
 class MainFragment : NikeFragment() {
     val mainViewModel: MainViewModel by viewModel()
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,9 +44,8 @@ class MainFragment : NikeFragment() {
         mainViewModel.bannerLiveData.observe(viewLifecycleOwner) {
             Timber.i(it.toString())
             val bannerSliderAdapter = BannerSliderAdapter(this, it)
-            val bannerSliderViewPager = view.findViewById<ViewPager2>(R.id.bannerSliderViewPager)
-            bannerSliderViewPager.adapter = bannerSliderAdapter
-
+            binding.bannerSliderViewPager.adapter = bannerSliderAdapter
+            binding.dotsIndicator.setViewPager2(binding.bannerSliderViewPager)
         }
     }
 }
